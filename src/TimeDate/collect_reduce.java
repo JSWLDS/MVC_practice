6ced42b3811f7695a096e6720cf55f79aaba6d2b
JSW <jswlds2@gmail.com>
2023-05-18 14:05:45 +0900

    intelij

--- src/TimeDate/collect_reduce.java
+++ src/TimeDate/collect_reduce.java
@@ -12,37 +12,19 @@ import java.util.stream.IntStream;
 public class collect_reduce {
     public static List<String> workTimeDate(String startDate, String endDate, String startTime, String endTime) {
 
-//        Stream<Integer> numbers = Stream.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
-//        Integer sum = numbers.reduce(10, (x, y) -> x + y);
-//        System.out.println("sum: " + sum);
-
-
         GetDateTime getDateTime = new GetDateTime();
         List<LocalTime> timeList =  getDateTime.getDateRandomTime(startTime, endTime, startDate, endDate);
         List<LocalDate> dateList = getDateTime.getDatesBetweenTwoDates(startDate, endDate);
 
         DateTimeFormatter formatter =  DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
 
-//        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm");
-
         List<LocalDateTime> dateTimeList = IntStream.iterate(0, i -> i+1)
                 .limit(dateList.size())
                 .mapToObj(i -> LocalDateTime.of(dateList.get(i), timeList.get(i)))
                 .collect(Collectors.toList());
 
-
-//        System.out.println(timeList.size());
-//        System.out.println(dateList.size());
-//        for(int i=0; i<dateList.size(); i++) {
-//            System.out.println(dateTimeList.get(i));
-//        }
-//        String str =  dateTimeList.get(0).format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm"));
-
         List<String> dateTimeStringList = dateTimeList.stream().map(t -> t.format(formatter)).collect(Collectors.toList());
 
-//        for(int i=0; i<dateList.size(); i++) {
-//            System.out.println(dateTimeStringList.get(i));
-//        }
         return dateTimeStringList;
     }
 
